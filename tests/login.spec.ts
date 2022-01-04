4fbf3fa6a3e8ac399e57766a84a3a4c07056b9d9
Mictim <mictim2002@gmail.com>
2022-01-04 16:17:55 +0100

    Added data driven test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,7 +2,8 @@ import { expect } from "@playwright/test";
 import { User } from "../src/dto/User";
 import test from "../src/fixtures/LoginFixture";
 
-test.describe('Login Tests', async() => {
+test.describe.parallel('Login Tests', async() => {
+  const users: User[] = [User.useRandomUser(), User.useRandomUser(), User.useRandomUser()];
     test("Sign In Test", async ({ page, signInSteps }) => {
         const user = new User("", "mj_raid2002@gmail.com", "qazwsx123");
         await page.goto("/");
@@ -13,13 +14,15 @@ test.describe('Login Tests', async() => {
         await expect(page.locator("a:has-text('Your Feed')")).toHaveClass(/active/);
       });
 
-      test(`Sign Up Test`, async ({page, signUpSteps}) => {
-        const user = User.useRandomUser();
-        await page.goto("/");
+      users.forEach(user => {
+        test(`Sign Up Test for user: ${user.getUsername()}`, async ({page, signUpSteps}) => {
+          await page.goto("/");
+        
+          await signUpSteps.openSignUpPageStep();
+          await signUpSteps.completeNewUserCreationStep(user);
       
-        await signUpSteps.openSignUpPageStep();
-        await signUpSteps.completeNewUserCreationStep(user);
-    
-        await expect(page.locator("a:has-text('Your Feed')")).toHaveClass(/active/);
+          await expect(page.locator("a:has-text('Your Feed')")).toHaveClass(/active/);
+        });
       });
+      
 })
